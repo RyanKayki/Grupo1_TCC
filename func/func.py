437aeec1b9124ab75b20417523f0e7ec0932aae0
Adillan07 <adillan.soares.senai@gmail.com>
2024-10-11 15:46:50 -0300

    Edição da tela principal dos funcionários

--- func/func.py
+++ func/func.py
@@ -1,14 +1,36 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect, send_from_directory
 from session.session import verifica_sessao
+from connection.connection import conecta_database  # Importando corretamente
+import os
 
 func_blueprint = Blueprint("func", __name__, template_folder="templates")
 
 # Rota para a página inicial dos funcionários
 @func_blueprint.route('/funcHome')
 def func_home():
-    title = "Funcionários"
-    login = verifica_sessao()
-    return render_template("funcHome.html", title=title, login=login)
+    if verifica_sessao():
+        try:
+            conexao = conecta_database()  # Agora chama a função aqui, no momento apropriado
+            cursor = conexao.cursor(dictionary=True)
+
+            query = """
+                SELECT c.descChamado, c.dataChamado, l.nomeLocal, i.nomeItem, c.imgChamado, s.nomeStatus, c.idItem, c.idLocal, r.descResposta
+                FROM chamado c
+                JOIN local l ON c.idLocal = l.idLocal
+                JOIN item i ON c.idItem = i.idItem
+                JOIN status s ON c.idStatus = s.idStatus
+                JOIN resposta r ON c.idChamado = r.idChamado
+            """
+            cursor.execute(query)
+            chamados = cursor.fetchall()
+            title="Manutenção"
+
+
+            return render_template("funcHome.html", chamados=chamados, title=title, login=True)
+        finally:
+            conexao.close()
+    else:
+        return redirect("/login")
 
 # Rota para cadastro de funcionário (lógica a ser implementada)
 @func_blueprint.route("/cadastro_funcionario", methods=['POST'])
