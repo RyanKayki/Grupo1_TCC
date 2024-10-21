8797491bd38c9aa255832cbef5dd7d9caf1c344b
Adillan07 <adillan.soares.senai@gmail.com>
2024-10-21 17:36:29 -0300

    Criação da rota de detalhes do chamado

--- func/func.py
+++ func/func.py
@@ -32,6 +32,32 @@ def func_home():
     else:
         return redirect("/login")
 
+# Rota para ver detalhes do chamado
+@func_blueprint.route('/detalhe_chamado/<int:idChamado>')
+def detalhe_chamado(idChamado):
+        if verifica_sessao():
+            try:
+                conexao = conecta_database()  # Agora chama a função aqui, no momento apropriado
+                cursor = conexao.cursor(dictionary=True)
+                query = """
+                    SELECT c.descChamado, c.concChamado, c.imgChamado, l.nomeLocal, i.nomeItem, s.nomeStatus, r.descResposta, r.dataResposta
+                    FROM chamado c
+                    JOIN local l ON c.idLocal = l.idLocal
+                    JOIN item i ON c.idItem = i.idItem
+                    JOIN status s ON c.idStatus = s.idStatus
+                    JOIN resposta r ON c.idChamado = r.idChamado
+                    WHERE c.idChamado = %s
+                """
+                cursor = conexao.execute(query, (idChamado,))
+                infos = cursor.fetchone()
+                title = "Detalhes do Chamado"
+                return render_template('details.html', title=title, infos=infos, login=True)
+            finally:
+                 conexao.close()
+        else:
+            return redirect("/login")
+        
+
 # Rota para cadastro de funcionário (lógica a ser implementada)
 @func_blueprint.route("/cadastro_funcionario", methods=['POST'])
 def cadastro_funcionario_json():
