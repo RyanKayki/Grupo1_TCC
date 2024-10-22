e9c2a06b017a19a42791abc6d88c821e1f460bc8
Adillan07 <adillan.soares.senai@gmail.com>
2024-10-22 15:57:30 -0300

    Edição da página de detalhes dos chamados

--- func/func.py
+++ func/func.py
@@ -5,6 +5,8 @@ import os
 
 func_blueprint = Blueprint("func", __name__, template_folder="templates")
 
+IMG_FOLDER = os.path.join('src', 'img')
+
 # Rota para a página inicial dos funcionários
 @func_blueprint.route('/funcHome')
 def func_home():
@@ -14,7 +16,7 @@ def func_home():
             cursor = conexao.cursor(dictionary=True)
 
             query = """
-                SELECT c.descChamado, c.dataChamado, l.nomeLocal, i.nomeItem, c.imgChamado, s.nomeStatus, c.idItem, c.idLocal, r.descResposta
+                SELECT c.descChamado, c.dataChamado, l.nomeLocal, i.nomeItem, c.imgChamado, s.nomeStatus, c.idItem, c.idLocal, r.descResposta, c.idChamado
                 FROM chamado c
                 JOIN local l ON c.idLocal = l.idLocal
                 JOIN item i ON c.idItem = i.idItem
@@ -48,10 +50,11 @@ def detalhe_chamado(idChamado):
                     JOIN resposta r ON c.idChamado = r.idChamado
                     WHERE c.idChamado = %s
                 """
-                cursor = conexao.execute(query, (idChamado,))
-                infos = cursor.fetchone()
+                cursor.execute(query, (idChamado,))
+                chamado = cursor.fetchone()
+                print(chamado)
                 title = "Detalhes do Chamado"
-                return render_template('details.html', title=title, infos=infos, login=True)
+                return render_template('details.html', title=title, chamado=chamado, login=True)
             finally:
                  conexao.close()
         else:
@@ -69,3 +72,9 @@ def cadastro_funcionario_json():
 def perfil_func():
     # Implementar lógica da tela de perfil
     pass
+
+# Rota para verficar imagem do chamado
+@func_blueprint.route('/img/chamados/<path:filename>')
+def serve_image(filename):
+    return send_from_directory(IMG_FOLDER, filename)
+
